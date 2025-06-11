b59e4275d13e03e0f10b1d44c22790aa3824b168
soom1017 <min001017@hanyang.ac.kr>
2025-06-12 04:20:25 +0900

    feat: val (train) set으로 kaisteval.py 성능평가 & 제출용 json 생성

--- runs/cmd/run_val.sh
+++ runs/cmd/run_val.sh
@@ -7,6 +7,17 @@ python val_simple.py \
     --device "$DEVICE" \
     --name "$RUN_NAME" \
     --task test \
+    --save-json \
+    --verbose \
+    --single-cls \
+    --rgbt
+
+python val_simple.py \
+    --weights "runs/train/$RUN_NAME/weights/best.pt" \
+    --data data/kaist-rgbt.yaml \
+    --device "$DEVICE" \
+    --name "$RUN_NAME" \
+    --task val \
     --verbose \
     --single-cls \
     --rgbt
